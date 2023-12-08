7e85c98281c8973a6c33b5864e9ee8e5479e985b
ytnoo <ytnoo@MSI>
2023-12-08 17:36:42 +0700

    Добавьте файлы проекта.

---
+++ Model/Game.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TicTacToeWPF_MVVM.Model
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; } = new List<Player>();
+        public string CurrentMark { get; set; }
+        private List<string> MarksToAssign { get; set; } = new List<string>();
+        public string Winner { get; set; }
+        public bool IsFinished { get; set; }
+
+        public Game(Player p1, Player p2)
+        {
+            // установка значений по умолчанию
+            IsFinished = false;
+
+            // добавление игроков в список
+            Players.Add(p1);
+            Players.Add(p2);
+
+            // присвоение отметок списку, а затем распределение их по игрокам
+            MarksToAssign.Add("X");
+            MarksToAssign.Add("O");
+            AssignMark();
+
+            // распечатать информацию об игроках
+            foreach (var player in Players)
+            {
+                Console.WriteLine($"PlayerInfo: {player.Name}, {player.Mark}, {player.IsTurn}");
+            }
+
+            // назначение хода игроку с помощью 'X'
+            AssignTurn();
+
+            foreach (var player in Players)
+            {
+                Console.WriteLine($"Информация об игроке после назначения хода: {player.Name}, {player.Mark}, {player.IsTurn}");
+            }
+        }
+
+        // покажите, чей это раунд
+        public string WhoseRound()
+        {
+            var player = Players.First(x => x.IsTurn == false);
+
+            return $"Ход игрока: {player.Name}, он играет {player.Mark}";
+        }
+
+        // изменить поворот на противоположное состояние при вызове
+        private void ChangeTurn(Player player)
+        {
+            player.IsTurn = !player.IsTurn;      
+        }
+
+        // способ управления игрой, переключения игрока, проверки выигрыша и т.д.
+        public void ChangeTurns(string[] field)
+        {
+            foreach (var player in Players)
+            {
+                if (CheckWin(field) == true)
+                {
+                    var winner = Players.FirstOrDefault(x => x.IsWinner == true);
+                    MessageBox.Show($"Выиграл игрок {winner.Name}, он играл {winner.Mark}");
+                    IsFinished = true;
+                    break;
+                }
+                // или ничья?
+                else if (IsTie(field))
+                {
+                    MessageBox.Show("Ничья!");
+                    IsFinished = true;
+                    break;
+                }
+
+                if (player.IsTurn)
+                {
+                    CurrentMark = player.Mark;
+                }
+
+                // измените поворот в конце
+                ChangeTurn(player);
+            }
+        }
+
+        // назначьте первый раунд парню с отметкой "X"
+        private void AssignTurn()
+        {
+            var player = Players.FirstOrDefault(x => x.Mark == "X");
+            player.IsTurn = true;
+        }
+
+        private bool CheckWin(string[] table)
+        {
+            var conditionToWin1 = CheckWinHorizontal(table);
+            var conditionToWin2 = CheckWinVertical(table);
+            var conditionToWin3 = CheckWinDiagonal(table);
+
+            return conditionToWin1 || conditionToWin2 || conditionToWin3;
+        }
+
+        // проверьте выигрыш по диагонали
+        private bool CheckWinDiagonal(string[] table)
+        {
+            // логика здесь такова: если 3 отметки совпадают, то победителем становится игрок с такой же отметкой
+            if ((table[0] + table[4] + table[8]) == "XXX")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            if ((table[0] + table[4] + table[8]) == "OOO")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            if ((table[2] + table[4] + table[6]) == "XXX")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            if ((table[2] + table[4] + table[6]) == "OOO")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        // проверьте выигрыш по вертикали
+        private bool CheckWinVertical(string[] table)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string txt = table[i] + table[i + 3] + table[i + 6];
+                if (txt == "XXX")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                    winner.IsWinner = true;
+                    
+                    return true;
+                }
+
+                if (txt == "OOO")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                    winner.IsWinner = true;
+
+                    return true;
+                }
+                    
+            }
+
+            return false;
+        }
+
+        // проверьте выигрыш по горизонтали
+        private bool CheckWinHorizontal(string[] table)
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                string txt = table[i] + table[i + 1] + table[i + 2];
+                if (txt == "XXX")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                    winner.IsWinner = true;
+
+                    return true;
+                }
+
+                if (txt == "OOO")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                    winner.IsWinner = true;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsTie(string[] table)
+        {
+            var txt = "";
+
+            foreach(var element in table)
+            {
+                txt += element;
+            }
+
+            if (txt.Length == 9 && !String.IsNullOrWhiteSpace(txt))
+                return true;
+            
+            return false;
+        }
+
+        // случайным образом присвойте игрокам одну из двух отметок
+        // метод, вызываемый в самом начале
+        private void AssignMark()
+        {
+            var random = new Random();
+
+            foreach (var player in Players)
+            {
+                int index = random.Next(MarksToAssign.Count);
+                player.Mark = MarksToAssign.ElementAt(index);
+                MarksToAssign.RemoveAt(index);
+            }
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeWPF_MVVM.Model;
+
+namespace TicTacToeWPF_MVVM.ViewModel
+{
+    using BaseClass;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    public class MainViewModel : ViewModel
+    {
+        // инициализация игрового объекта
+        private Game _ngame;
+
+        // вспомогательный флаг для сброса состояния
+        private bool _gameInProgress = false;
+
+        // создание массива пустых строк
+        // 1x9
+        public string[] _table = new string[] { "", "", "", "", "", "", "", "", "" };
+
+        public string[] Table
+        {
+            get { return _table; }
+            set
+            {
+                _table = value;
+                onPropertyChange(nameof(Table));
+            }
+        }
+
+        // полная опора для отображения информации под доской
+        private string _whoseRound;
+
+        public string WhoseRound
+        {
+            get
+            {
+                return _whoseRound;
+            }
+            set
+            {
+                _whoseRound = value;
+                onPropertyChange(nameof(WhoseRound));
+            }
+        }
+
+
+        public MainViewModel()
+        {
+        }
+
+        // поля для ввода имени первого игрока
+        private string _playerName1;
+
+        public string PlayerName1
+        {
+            get { return _playerName1; }
+            set
+            {
+                _playerName1 = value;
+                onPropertyChange(nameof(PlayerName1));
+            }
+        }
+
+        // поле для ввода имени второго игрока
+        private string _playerName2;
+
+        public string PlayerName2
+        {
+            get { return _playerName2; }
+            set
+            {
+                _playerName2 = value;
+                onPropertyChange(nameof(PlayerName2));
+            }
+        }
+
+        // если можно щелкнуть, то запускается функция OnClick
+        private ICommand _onClick;
+
+        public ICommand OnClick
+        {
+            get
+            {
+                return _onClick ?? (_onClick = new RelayCommand(OnClickFunc, CanOnClick));
+            }
+        }
+
+        private void OnClickFunc(object obj)
+        {
+            // команда синтаксического анализа кнопки для преобразования в int
+            int field = int.Parse((string)obj);
+
+            Table[field] = _ngame.CurrentMark;
+
+            // обновить таблицу
+            Table = Table;
+
+            // измените поворот после щелчка
+            _ngame.ChangeTurns(Table);
+            WhoseRound = _ngame.WhoseRound();
+
+            // сбросить поле, если игра закончена (кто-то выиграл или сыграл вничью)
+            if (_ngame.IsFinished)
+            {
+                Table = new string[] { "", "", "", "", "", "", "", "", "" };
+                PlayerName1 = "";
+                PlayerName2 = "";
+                _gameInProgress = false;
+                WhoseRound = "";
+            }
+        }
+
+        // если на одной из маленьких кнопок есть что-то еще, кроме "", то не нажимайте на нее
+        private bool CanOnClick(object param)
+        {
+            // преобразуйте команду кнопки в int, чтобы она действовала как индекс
+            int field = int.Parse((string)param);
+
+            // заблокируйте все поля, если игра не запущена
+            if (!_gameInProgress)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Table.Length; i++)
+            {
+                if (Table[field] != "")
+                    return false;
+            }
+
+            return true;
+        }
+
+        private ICommand _start;
+
+        public ICommand Start
+        {
+            get
+            {
+                return _start ?? (_start = new RelayCommand(StartGame, CanStartGame));
+            }
+        }
+
+        // создаем игроков и игру после указания имен и нажатия кнопки
+        private void StartGame(object param)
+        {
+            var player1 = new Player(_playerName1);
+            var player2 = new Player(_playerName2);
+            _ngame = new Game(player1, player2);
+            _gameInProgress = true;
+
+            // выполнение поворота при старте, чтобы текущая отметка не была нулевой
+            _ngame.ChangeTurns(Table);
+
+         
+            WhoseRound = _ngame.WhoseRound();
+        }
+
+        // если указаны имена, то игру можно начинать
+        private bool CanStartGame(object param)
+        {
+            // если оба текстовых поля не пусты
+            var textBoxEmpty = !(String.IsNullOrEmpty(_playerName1) || String.IsNullOrEmpty(_playerName2));
+
+            // если оба они не пусты и игра не запущена, то вы можете нажать кнопку
+            return (textBoxEmpty && !_gameInProgress);
+        }
+    }
+}
